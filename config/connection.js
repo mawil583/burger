f11322f756a2710b5ed7d744f3fac5ab854b9462
Michael Williams <mwilliams2964@gmail.com>
2019-11-21 22:05:31 -0700

    deploying burger homework

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,26 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: `localhost`,
+    user: `root`,
+    port: 3306,
+    password: "hnfoiclbr2@cm",
+    database: `burgers_db`
+  })
+}
+//mysql://rnaf9z4tzvwsxqdj:p8mkqx4x908x2yh9@blonze2d5mrbmcgf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/r48p4bx8u5dwb7sz
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "hnfoiclbr2@cm",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "hnfoiclbr2@cm",
+//   database: "burgers_db"
+// });
 
 // Make connection.
 connection.connect(function(err) {
